6b48bddd28ef0cc910922a100d6f8d04ec58b6da
esmaiel <esi.karkhah@gmail.com>
2018-06-09 09:47:27 +0430

    sdsds

--- src/Views/backend/JsPanelBasicDataValue/content.blade.php
+++ src/Views/backend/JsPanelBasicDataValue/content.blade.php
@@ -23,9 +23,11 @@
             <div id="message_basic_data_value_insert" class="form_message_area"></div>
             <form id="FormInsertBasicDataValue" class="form-horizontal">
 
-                {{-- <div class="form-group col-xs-12">
-                    <label class="col-sm-3 text-center"><span
-                                class="red-star">*</span><span>والد:</span></label>
+                 <div class="form-group col-12 fg_basicdata_id">
+                    <label class="col-sm-3 text-center control-label label_title" for="basicdata_id">
+                        <span class="more_info"></span>
+                        <span class="red-star">*</span>
+                        <span>والد:</span></label>
                     <div class="col-sm-5">
                         <select class="form-control" name="basicdata_id" id="select_basicdata">
                           @if(isset($basicdata) && $basicdata)
@@ -35,8 +37,8 @@
                             @endif
                         </select>
                     </div>
-                    <div class="col-sm-4" id="#error_title"></div>
-                </div>--}}
+                     <div class="col-sm-4 messages"></div>
+                </div>
                 <div class="form-group col-xs-12 fg_title">
                     <label class="col-sm-3 text-center control-label label_title" for="title">
                         <span class="more_info"></span>
@@ -48,63 +50,65 @@
                     </div>
                     <div class="col-sm-4 messages"></div>
                 </div>
-                {{--
-                <div class="form-group col-xs-12">
-                    <label class="col-sm-3 text-center"><span
-                                class="red-star">*</span><span>عنوان مورد استفاده:</span>
+
+                <div class="form-group col-xs-12 fg_dev_title">
+                    <label class="col-sm-3 text-center control-label label_title" for="dev_title">
+                        <span class="more_info"></span>
+                        <span class="red-star">*</span>
+                        <span>عنوان مورد استفاده:</span>
                     </label>
                     <div class="col-sm-5">
                         <input type="text" class="form-control" name="dev_title"/>
                     </div>
-                    <div class="col-sm-4" id="#error_dev_title"></div>
+                    <div class="col-sm-4 messages"></div>
                 </div>
-                <div class="form-group col-xs-12">
-                    <label class="col-sm-3 text-center">
-                        <span class="red-star"></span>
-                        <span>مقدار ثابت:</span>
-                    </label>
-                    <div class="col-sm-5">
-                        <input type="text" class="form-control" name="dev_val"/>
-                    </div>
-                    <div class="col-sm-4" id="#error_dev_val"></div>
-                </div>
-                <div class="form-group col-xs-12">
-                    <label class="col-sm-3 text-center"><span class="red-star"></span><span>توضیحات:</span></label>
-                    <div class="col-sm-5">
-                      <textarea type="text" class="form-control" name="comment"></textarea>
-                    </div>
-                    <div class="col-sm-4" id="#error_comment"></div>
-                </div>
-                <div class="form-group col-xs-12">
-                    <label class="col-sm-3 text-center">
-                        <span class="red-star"></span>
-                        <span>داده های بیشتر:</span></label>
-                    <div class="col-sm-5">
-                        <input  type="text" class="form-control" name="extra_field" id="extra_field"/>
-                    </div>
-                    <div class="col-sm-4" id="#error_title"></div>
-                </div>
-                <div class="form-group  col-xs-12">
-                    <label class="col-sm-3 text-center">
-                        <span class="red-star">*</span><span>وضعیت:</span>
-                    </label>
-                    <div class="col-sm-5 text-right">
-                        <input type="radio" name="is_active" value="1" checked/>
-                        <span> فعال </span>
-                        <input type="radio" name="is_active" value="0"/>
-                        <span> غیرفعال </span>
-                    </div>
-                    <div class="col-sm-4" id="#error_msg_is_active"></div>
-                </div>
-                <div class="form-group col-xs-12">
-                    <label class="col-sm-3"></label>
-                    <div class="col-sm-5 text-right">
-                        <input type="hidden" class="btn btn-success" id="submit_insert_basicdata_value" value="ثبت"/>
-                        <div class="messsage"></div>
-                    </div>
-                    <div class="col-sm-4"></div>
-                </div>
-                --}}
+                {{--<div class="form-group col-xs-12">
+                   <label class="col-sm-3 text-center">
+                       <span class="red-star"></span>
+                       <span>مقدار ثابت:</span>
+                   </label>
+                   <div class="col-sm-5">
+                       <input type="text" class="form-control" name="dev_val"/>
+                   </div>
+                   <div class="col-sm-4" id="#error_dev_val"></div>
+               </div>
+               <div class="form-group col-xs-12">
+                   <label class="col-sm-3 text-center"><span class="red-star"></span><span>توضیحات:</span></label>
+                   <div class="col-sm-5">
+                     <textarea type="text" class="form-control" name="comment"></textarea>
+                   </div>
+                   <div class="col-sm-4" id="#error_comment"></div>
+               </div>
+               <div class="form-group col-xs-12">
+                   <label class="col-sm-3 text-center">
+                       <span class="red-star"></span>
+                       <span>داده های بیشتر:</span></label>
+                   <div class="col-sm-5">
+                       <input  type="text" class="form-control" name="extra_field" id="extra_field"/>
+                   </div>
+                   <div class="col-sm-4" id="#error_title"></div>
+               </div>
+               <div class="form-group  col-xs-12">
+                   <label class="col-sm-3 text-center">
+                       <span class="red-star">*</span><span>وضعیت:</span>
+                   </label>
+                   <div class="col-sm-5 text-right">
+                       <input type="radio" name="is_active" value="1" checked/>
+                       <span> فعال </span>
+                       <input type="radio" name="is_active" value="0"/>
+                       <span> غیرفعال </span>
+                   </div>
+                   <div class="col-sm-4" id="#error_msg_is_active"></div>
+               </div>
+               <div class="form-group col-xs-12">
+                   <label class="col-sm-3"></label>
+                   <div class="col-sm-5 text-right">
+                       <input type="hidden" class="btn btn-success" id="submit_insert_basicdata_value" value="ثبت"/>
+                       <div class="messsage"></div>
+                   </div>
+                   <div class="col-sm-4"></div>
+               </div>
+               --}}
                 <div class="form-group fg_submit col-xs-12">
                     <div class="col-sm-5 col-sm-offset-3">
                         <button type="submit" name="submit" id="submit_insert_basicdata_value" class="btn btn-success">ثبت وتایید</button>
