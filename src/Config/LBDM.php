13ff982b4e96254bbbccee12d902295bd33b86c4
adel <adel.raheli@gmail.com>
2018-10-05 00:21:06 +0330

    middleware .env as array supported

--- src/Config/LBDM.php
+++ src/Config/LBDM.php
@@ -2,28 +2,14 @@
 
 return [
 
-	/* Important Settings */
+    /* Important Settings */
 
-	// ======================================================================
-	// never remove 'web', . just put your middleware like auth or admin (if you have) here. eg: ['web','auth']
-	'private_middlewares' => ['web'],
-	'public_middlewares' => ['web'],
-	// you can change default route from sms-admin to anything you want
-	'private_route_prefix' => 'LBDM',
-	'public_route_prefix' => 'LBDM',
-	// SMS.ir Api Key
-	'api-key' => env('SMSIR-API-KEY','Your api key'),
-	// SMS.ir Secret Key
-	'secret-key' => env('SMSIR-SECRET-KEY','Your secret key'),
-	// Your sms.ir line number
-	'line-number' => env('SMSIR-LINE-NUMBER','Your Sms.ir Line Number'),
-	// ======================================================================
-
-	// set true if you want log to the database
-	'db-log' => true,
-
-	/* Admin Panel Title */
-	'title' => 'مدیریت پیامک ها',
-	// How many log you want to show in sms-admin panel ?
-	'in-page' => '15'
+    // ======================================================================
+    // never remove 'web', . just put your middleware like auth or admin (if you have) here. eg: ['web','auth']
+    'private_middlewares'  => explode(',', env('BACKEND_LBDM_MIDDLEWARES', 'web')),
+    'public_middlewares'   => explode(',', env('FRONTEND_LBDM_MIDDLEWARES', 'web')),
+    // you can change default route from sms-admin to anything you want
+    'private_route_prefix' => 'LBDM',
+    'public_route_prefix'  => 'LBDM',
+    // ======================================================================
 ];
