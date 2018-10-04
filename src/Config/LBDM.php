cf37fccb6c4446c7a860af19f38e66ad6579bf12
adel <adel.raheli@gmail.com>
2018-10-05 00:36:42 +0330

    config change and fixed issue

--- src/Config/LBDM.php
+++ src/Config/LBDM.php
@@ -6,10 +6,10 @@ return [
 
     // ======================================================================
     // never remove 'web', . just put your middleware like auth or admin (if you have) here. eg: ['web','auth']
-    'private_middlewares'  => explode(',', env('BACKEND_LBDM_MIDDLEWARES', 'web')),
-    'public_middlewares'   => explode(',', env('FRONTEND_LBDM_MIDDLEWARES', 'web')),
+    'private_middlewares'  => explode(',', env('LBDM_BACKEND_MIDDLEWARES', 'web')),
+    'public_middlewares'   => explode(',', env('LBDM_FRONTEND_MIDDLEWARES', 'web')),
     // you can change default route from sms-admin to anything you want
-    'private_route_prefix' => 'LBDM',
-    'public_route_prefix'  => 'LBDM',
+    'private_route_prefix' => env('LBDM_BACKEND_ROUTE_PREFIX', 'LBDM'),
+    'public_route_prefix'  => env('LBDM_FRONTEND_ROUTE_PREFIX', 'LBDM'),
     // ======================================================================
 ];
